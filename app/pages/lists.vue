95fe6b2f5e148f893cd31bfb1d0da982cd4d4634
ax <ax@ax.ax>
2017-07-03 22:42:32 +0300

    init

---
+++ app/pages/lists.vue
@@ -0,0 +1,74 @@
+<template>
+  <f7-page>
+    <f7-fab color="pink"
+            @click="addItem">
+      <f7-icon icon="icon-plus"></f7-icon>
+    </f7-fab>
+
+    <f7-navbar back-link="Back"
+               :title="'Dict '+key"
+               sliding></f7-navbar>
+    <!-- <f7-block-title co>List</f7-block-title> -->
+    <f7-list contacts>
+      <f7-list-item swipeout
+                    :link="'/edit#'+key+'/'+item['.key']"
+                    :key="item['.key']"
+                    v-for="item in items"
+                    :title="item.name || item.title"
+                    :badge="item['.key']"
+                    badge-color="red">
+        <f7-swipeout-actions>
+          <f7-swipeout-button delete
+                              @click="removeItem(item)">Delete</f7-swipeout-button>
+        </f7-swipeout-actions>
+      </f7-list-item>
+    </f7-list>
+
+  </f7-page>
+</template>
+<script>
+  console.log('list.vue', this)
+
+  export default {
+    name: 'FirebaseList',
+
+    data: function () {
+      return {
+        key: ''
+      }
+    },
+
+    firebase: {
+      items: window.firebase.database().ref('null')
+    },
+
+    watch: {
+      key: function (value, oldValue) {
+        if (value === oldValue) {
+          return
+        }
+
+        console.log('key changed', value, oldValue)
+        this.$firebaseRefs && this.$firebaseRefs.items && this.$unbind('items')
+        this.$bindAsArray('items', window.firebase.database().ref(value))
+      }
+    },
+
+    methods: {
+      addItem: function () {
+        console.log('add')
+        this.$firebaseRefs.items.push({
+          name: 'ax'
+        })
+      },
+      removeItem: function (item) {
+        this.$firebaseRefs.items.child(item['.key']).remove()
+      }
+    },
+
+    beforeMount: function () {
+      this.key = this.$route.hash
+    }
+  }
+
+</script>
